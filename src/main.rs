2acde3f9b85f8565f00eead88e95aa5d2c744560
vagrant <vagrant@ubuntu1804.localdomain>
2023-11-29 21:35:00 -0800

    Implemented event_camera

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,57 @@
-use rand::{random, Rng};
+use std::time::{Instant};
+use rand::{random};
 
-struct PowerDistributionSimulator {
-    solar_i_v: f32,
-    solar_i_c: f32,
-    battery_v: f32,
-    battery_c: f32,
-
-    rail_3_v: f32,
-    rail_3_c: f32,
-    rail_5_v: f32,
-    rail_5_c: f32,
-    rail_12_v: f32,
-    rail_12_c: f32,
-}
-
-
-impl PowerDistributionSimulator {
-    // Constructor remains public
-    pub fn new() -> PowerDistributionSimulator {
-        let mut simulator = PowerDistributionSimulator {
-            solar_i_v: 5.0,
-            solar_i_c: 0.0,
-
-            battery_v: 40.0,
-            battery_c: 0.0,
-
-            rail_3_v: 3.3,
-            rail_5_v: 5.0,
-            rail_12_v: 12.0,
-
-            rail_3_c: 0.0,
-            rail_5_c:0.0,
-            rail_12_c:0.0,
-        };
-        // numbers i pulled out of my ass
-        simulator.rail_3_c = simulator.random_float(0.0, 1.2);
-        // numbers i pulled out of my ass
-        simulator.rail_5_c = simulator.random_float(0.0, 0.9);
-        // more numbers out of my ass
-        simulator.rail_12_c = simulator.random_float(0.0, 30.0);
-
-        // i cannot emphasize further that these numbers are magic as shit
-        simulator.solar_i_c = 5.0;
-        simulator.battery_c = 12.0;
-
-
-        simulator
-    }
-    // I cant be fucked making 6 more classes to return V and A
-    pub fn get_3_rail_power(&mut self) -> f32{
-        self.rail_3_v * self.rail_3_c
-    }
-    pub fn get_5_rail_power(&mut self) -> f32{
-        self.rail_5_v * self.rail_5_c
-    }
-    pub fn get_12_rail_power(&mut self) -> f32{
-        self.rail_12_v * self.rail_12_c
-    }
-    pub fn get_battery_power(&mut self) -> f32{
-        self.battery_v * self.battery_c
-    }
-
-    pub fn get_solar_power(&mut self) -> f32{
-        self.solar_i_v * self.solar_i_c
-    }
-
+trait Component{
+    fn get_name(& self) -> &String;
+    fn get_power_draw(&mut self) -> f32;
+    fn get_current(& mut self) -> f32;
+    fn get_voltage(& mut self) -> f32;
     fn random_float(&mut self, min_range:f32, max_range:f32) -> f32 {
         return (random::<f32>() * (max_range - min_range)) + min_range;
     }
 }
 
-// Usage example
+struct EventCamera {
+    component_name: String,
+    rail_voltage: f32,
+    power_loss: f32,
+    last_update: Instant,
+}
+
+impl EventCamera {
+    pub fn new() -> EventCamera {
+        EventCamera {
+            component_name: String::from("Event Camera"),
+            rail_voltage: 5.0,
+            power_loss: 0.09,
+            last_update: Instant::now(),
+        }
+    }
+
+}
+impl Component for EventCamera {
+    fn get_name(&self) -> &String{
+        &self.component_name
+    }
+
+    fn get_power_draw(&mut self) -> f32 {
+        let now = Instant::now();
+        let elapsed = now.duration_since(self.last_update).as_secs_f32(); // Get the elapsed time in seconds
+        self.last_update = now; // Update the last update time
+
+        (3.0 / 5.0) * ((elapsed / 5.0).cos() + 1.0) + self.power_loss + 0.3
+    }
+
+    fn get_current(&mut self) -> f32 {
+        self.get_power_draw() / self.rail_voltage
+    }
+
+    fn get_voltage(&mut self) -> f32{
+        self.rail_voltage
+    }
+}
+
 fn main() {
-    let mut connector = PowerDistributionSimulator::new();
-    println!("{}w", connector.get_12_rail_power())
+    let mut camera = EventCamera::new();
+    println!("{}w", camera.get_power_draw());
 }
